91af469334cabb72351d42eb9bf98c38256c1ab0
Mirzafatur <mirzafatur97@gmail.com>
2024-06-25 22:18:44 +0700

    feat "add Header"

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,24 @@ import { useState } from "react";
 import "./index.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="header">
+        <div className="greeting">
+          <h1>Good Morning!</h1>
+          <p>Let's set your to-do list for today 🏆</p>
+        </div>
+        <div className="action">
+          <select>
+            <option value="input">Sort by Input</option>
+            <option value="priority">Sort by Priority</option>
+            <option value="finished">Sort by Finished Status</option>
+          </select>
+          <button>Clear Todo</button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
